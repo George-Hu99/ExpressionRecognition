53943dae8878225d224c97da7ecbf8ad9fca77c4
Mr.Sheng <3165407980@qq.com>
2021-01-16 10:01:59 +0800

    :tada:

---
+++ src/main/java/cn/edu/gdut/FacialExpressionRecognition/FacialExpressionRecognitionApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.gdut.FacialExpressionRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialExpressionRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialExpressionRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/gdut/FacialExpressionRecognition/FacialExpressionRecognitionApplicationTests.java
@@ -0,0 +1,12 @@
+package cn.edu.gdut.FacialExpressionRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialExpressionRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
